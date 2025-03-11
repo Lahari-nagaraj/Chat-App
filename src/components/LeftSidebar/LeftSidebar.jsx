606bede6241ffd7a4287dc2240f1db4d12d0199d
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2025-03-11 06:25:29 +0530

    updated leftslidebar

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -17,13 +17,15 @@ function LeftSidebar() {
             </div>
         </div>
 <div className="ls-list">
-    <div className="friends">
+    {Array(12).fill("").map((item,index)=>(
+        <div key={index} className="friends">
         <img src={assets.profile_img} alt="" />
         <div className="">
             <p>Richard Sanford</p>
             <span>Hello, How are you?</span>
         </div>
     </div>
+    ))}
 </div>
     </div>
   )
