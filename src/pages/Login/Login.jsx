4fd15d06253d9b8e0179ab1a908f6493d884e59f
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2025-03-12 07:52:59 +0530

    started backend

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 import './Login.css';
 import assets from '../../assets/assets'
+import { signup } from '../../config/firebase';
 
 const Login = () => {
   const [currState,setCurrState] = useState("Sign Up");
+  const [userName, setUserName] = useState("");
+  const[email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className="logo" />
       <form className='login-form'>
         <h2>{currState}</h2>
-        {currState === "Sign Up"?<input type="text" placeholder="Username" className="form-input" required />:null}
-        <input type="email" placeholder="Email address"className="form-input" required/>
-        <input type="password" placeholder="Password"className="form-input" required />
+        {currState === "Sign Up"?<input onChange={(e)=>setUserName(e.target.value)} value={userName} type="text" placeholder="Username" className="form-input" required />:null}
+        <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder="Email address"className="form-input" required/>
+        <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder="Password"className="form-input" required />
         <button type='submit'>{currState === "Sign Up"?"Create account":"Login"}</button>
         <div className="login-term">
           <input type="checkbox" />
@@ -23,8 +29,6 @@ const Login = () => {
             ? <p className='login-toggle'>Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>
             :<p className='login-toggle'>Create an account <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
           }
-         
-          
         </div>
       </form>
       </div>
