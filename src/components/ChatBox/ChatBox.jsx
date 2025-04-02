952d8ddfbd1965ddbdd34c12ee73dee3c726a3d5
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2025-04-02 22:59:15 +0530

    updated chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,58 +1,118 @@
-import React from 'react'
-import assets from '../../assets/assets'
-import "./ChatBox.css"
+import React, { useContext, useEffect, useState } from "react";
+import assets from "../../assets/assets";
+import "./ChatBox.css";
+import { AppContext } from "../../context/AppContext";
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
 
 const ChatBox = () => {
-  return (
+  const {userData, messagesId,chatUser,messages,setMessages}=useContext(AppContext);
+
+  const [input,setInput]  = useState("");
+
+  const sendMessage = async () => {
+    try{
+      if(input && messagesId){
+        await updateDoc(doc(db,"messages",messagesId),{
+          messages:arrayUnion({
+            sId:userData.id,
+            text:input,
+            createdAt: new Date()
+          })
+        })
+        const userIDs = [chatUser.rId,userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db,"chats",id);
+          const userChatsSnapshot= await getDoc(userChatsRef);
+
+          if(userChatsSnapshot.exists()){
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex((c)=>c.messageId === messagesId);
+            userChatData.chatsData[chatIndex].lastMessage = input.slice(0,30);
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if(userChatData.chatsData[chatIndex].rId === userData.id){
+              userChatData.chatsData[chatIndex].messageSeen = false
+            }
+            await updateDoc(userChatsRef,{
+              chatsData:userChatData.chatsData
+            })
+          }
+        })
+      }
+    }catch(error){
+      toast.error(error.message)
+    }
+    setInput("");
+  }
+
+  const convertTimestamp = (timestamp)=> {
+    let date = timestamp.toDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    if(hour>12){
+      return hour-12 +":"+minute + "PM";
+    }
+    else{
+      return hour  + ":" + minute + "AM";
+    }
+  }
+
+  useEffect(() =>{
+    if(messagesId){
+      const unSub = onSnapshot(doc(db,"messages",messagesId),(res) => {
+        setMessages(res.data().messages.reverse())
+        
+      })
+      return () =>{
+        unSub();
+      }
+    }
+  },[messagesId])
+
+  return chatUser ? (
     <div className="chat-box">
       <div className="chat-user">
-        <img src={assets.profile_img} alt="" />
+        <img src={chatUser.userData.avatar} alt="" />
         <p>
-          Richard Sanford <img className="dot" src={assets.green_dot} alt="" />
+          {chatUser.userData.name}
+          <img className="dot" src={assets.green_dot} alt="" />
         </p>
         <img src={assets.help_icon} className="help" alt="" />
       </div>
 
       <div className="chat-msg">
-        <div className="s-msg">
-          <p className="msg">
-            Lorem ipsum is placeholder text commonly used...
-          </p>
-          <div className="">
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
+        {messages.map((msg,index) => (
+           <div key={index} className={msg.sId === userData.id ? "s-msg" : "r-msg"}>
+             <p className="msg">
+              {msg.text}
+             </p>
+             <div className="">
+               <img src={msg.sId === userData.id ? userData.avatar: chatUser.userData.avatar} alt="" />
+               <p>{convertTimestamp(msg.createdAt)}</p>
+             </div>
+           </div>
+        ))}
+       
 
-        <div className="s-msg">
-          <img className="msg-img"src={assets.pic1} alt="" />
-          <div className="">
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
-
-        <div className="r-msg">
-          <p className="msg">
-            Lorem ipsum is placeholder text commonly used...
-          </p>
-          <div className="">
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
+        
       </div>
 
       <div className="chat-input">
-        <input type="text" placeholder="Send a message" />
+        <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Send a message" />
         <input type="file" id="image" accept="image/png, image/jpg" hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
-        <img src={assets.send_button} alt="" />
+        <img onClick={sendMessage} src={assets.send_button} alt="" />
       </div>
     </div>
+  ) : (
+    <div className="chat-welcome">
+      <img src={assets.logo_icon} alt="" />
+      <p>Chat anytime, anywhere</p>
+    </div>
   );
-}
+};
 
-export default ChatBox
+export default ChatBox;
